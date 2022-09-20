b6364e869bbf8b03a686f663d9c8891d4c4d6017
Yanis Berkani <yanis.berkani@epita.fr>
2022-09-20 16:52:00 +0200

    Partie 4

--- src/Launcher.java
+++ src/Launcher.java
@@ -6,10 +6,25 @@ public class Launcher {
         Scanner reader = new Scanner(System.in);
         String input = reader.nextLine();
         while (!"quit".equals(input)) {
-            System.out.println("Unknown command");
-	    input = reader.nextLine();
-        }
-        //System.out.println(str);
+		if (input.equals("fibo")) {
+			int n, a = 0, b = 0, c = 1;
+			Scanner s = new Scanner(System.in);
+			System.out.print("Enter value of n:");
+			n = s.nextInt();
+			for ( int i = 1; i <= n ; i++) {
+				a = b;
+				b = c;
+				c = a + b;
+			}	
+			System.out.print(a+" ");	
+			System.out.print("\n");
+			input = reader.nextLine();
+		}
+		if (!"fibo".equals(input) && !"quit".equals(input)) {
+			System.out.println("Unknown command");
+	    		input = reader.nextLine();
+		}
+	}
         reader.close();
-    }
+    }		
 }
